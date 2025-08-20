bc1cac827112a5c3ea6225a4d09926b7a4a0644d
Hashini-Bhagya <kavindihashini15@gmail.com>
2025-08-20 20:53:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './pages/contact';
+
+
+
+function App() {
+  return (
+    <Router>
+    
+      <Routes>
+        
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
